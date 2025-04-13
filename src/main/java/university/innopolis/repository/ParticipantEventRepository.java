011880cb5e9081507ed18146c85d194dee38cd6f
Efim Puzhalov <98216456+yeaphm@users.noreply.github.com>
2025-04-13 18:29:02 +0300

    participant functionality (#5)
    Co-authored-by: Efim Puzhalov <e.puzhalov@innopolis.universtiy>

--- src/main/java/university/innopolis/repository/ParticipantEventRepository.java
+++ src/main/java/university/innopolis/repository/ParticipantEventRepository.java
@@ -1,22 +1,11 @@
-//package university.innopolis.repository;
-//
-//import jakarta.persistence.*;
-//import university.innopolis.entity.Event;
-//import university.innopolis.entity.Participant;
-//import university.innopolis.entity.ParticipantEventId;
-//
-//@Entity
-//@IdClass(ParticipantEventId.class)
-//public class ParticipantEventRepository {
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "participant_id")
-//    private Participant participant;
-//
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "event_id")
-//    private Event event;
-//
-//    // Getters and setters
-//}
+package university.innopolis.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import university.innopolis.entity.ParticipantEvent;
+import university.innopolis.entity.ParticipantEventId;
+
+import java.util.List;
+
+public interface ParticipantEventRepository extends JpaRepository<ParticipantEvent, ParticipantEventId> {
+    List<ParticipantEvent> findByParticipantId(Long participantId);
+}
